f6456b5e8be367eac2ecea7a387b955131491ab2
Jonathan Lee <jonathan.lee.devel@gmail.com>
2022-02-18 19:43:30 +0000

    Added tests for generating password reset tokens

--- src/services/password/__tests__/generate-password-reset-token.spec.ts
+++ src/services/password/__tests__/generate-password-reset-token.spec.ts
@@ -1,10 +1,6 @@
 import {makeGeneratePasswordResetToken} from '../generate-password-reset-token';
-
-beforeAll(() => {
-});
-
-beforeEach(() => {
-});
+import {DEFAULT_EXPIRY_TIME_MINUTES, DEFAULT_TOKEN_SIZE}
+  from '../../../config/Token';
 
 describe('Generate password reset token', () => {
   it('When makeGeneratePasswordResetToken Then generatePasswordResetToken',
@@ -13,10 +9,25 @@ describe('Generate password reset token', () => {
         expect(generatePasswordResetToken).not.toBeNull();
         expect(generatePasswordResetToken).toBeInstanceOf(Function);
       });
-});
-
-afterEach(() => {
-});
-
-afterAll(() => {
+  it('When generatePasswordResetToken Then token of given length',
+      async () => {
+        const generatePasswordResetToken = makeGeneratePasswordResetToken();
+        const token = await generatePasswordResetToken(
+            DEFAULT_TOKEN_SIZE,
+            DEFAULT_EXPIRY_TIME_MINUTES,
+        );
+        const numberOfCharactersPerByte = 2;
+        expect(token.value.toString().length)
+            .toBe(DEFAULT_TOKEN_SIZE * numberOfCharactersPerByte);
+      });
+  it('When generatePasswordResetToken Then token not expired',
+      async () => {
+        const generatePasswordResetToken = makeGeneratePasswordResetToken();
+        const token = await generatePasswordResetToken(
+            DEFAULT_TOKEN_SIZE,
+            DEFAULT_EXPIRY_TIME_MINUTES,
+        );
+        expect(token.expiryDate.getTime())
+            .toBeGreaterThanOrEqual(new Date().getTime());
+      });
 });
