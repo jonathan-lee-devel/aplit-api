739689be02c69321f120fbc6a07bea22484a3acb
Jonathan Lee <jonathan.lee.devel@gmail.com>
2022-03-02 23:38:55 +0000

    Modified docker compose

--- src/config/Mail.ts
+++ src/config/Mail.ts
@@ -1,6 +1,9 @@
 import nodemailer, {Transporter} from 'nodemailer';
 import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
+// eslint-disable-next-line max-len
+console.log(`auth:{ user:${process.env.EMAIL_USER}, pass:${process.env.EMAIL_PASSWORD} }`);
+
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
