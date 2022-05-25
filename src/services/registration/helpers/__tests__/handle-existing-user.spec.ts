876c67b86edc3bd527ec006e30acb56a8ade4ff3
Jonathan Lee <jonathan.lee.devel@gmail.com>
2022-05-25 23:04:39 +0100

    Added tests to handle-existing-user.spec.ts in Registration Service

--- src/services/registration/helpers/__tests__/handle-existing-user.spec.ts
+++ src/services/registration/helpers/__tests__/handle-existing-user.spec.ts
@@ -1,12 +1,62 @@
 import {makeHandleExistingUser} from '../handle-existing-user';
 
 describe('Handle existing user', () => {
-  it('WHen makeHandleExistingUser Then handleExistingUser',
+  it('When makeHandleExistingUser Then handleExistingUser',
       async () => {
         // @ts-ignore
         const handleExistingUser = makeHandleExistingUser({});
 
         expect(handleExistingUser).not.toBeNull();
         expect(handleExistingUser).toBeInstanceOf(Function);
+      });
+  it('When handleExistingUser And No Existing User Then True',
+      async () => {
+        // @ts-ignore
+        const handleExistingUser = makeHandleExistingUser({
+          findOne: () => {
+            return null;
+          },
+        });
+
+        const result = await handleExistingUser('email');
+
+        expect(result).toBeTruthy();
+      });
+  it('When handleExistingUser and Existing User Email Verified Then False',
+      async () => {
+        const handleExistingUser = makeHandleExistingUser({
+          // @ts-ignore
+          findOne: () => {
+            return {
+              emailVerified: true,
+            };
+          },
+        });
+
+        const result = await handleExistingUser('email');
+
+        expect(result).not.toBeTruthy();
+      });
+  it('When handleExistingUser and Existing User Not Email Verified Then True',
+      async () => {
+        let findByIdAndDeleteCalled = false;
+        const handleExistingUser = makeHandleExistingUser({
+          // @ts-ignore
+          findOne: () => {
+            return {
+              emailVerified: false,
+            };
+          },
+          // @ts-ignore
+          findByIdAndDelete: () => {
+            findByIdAndDeleteCalled = true;
+          },
+        },
+        );
+
+        const result = await handleExistingUser('email');
+
+        expect(result).toBeTruthy();
+        expect(findByIdAndDeleteCalled).toBeTruthy();
       });
 });
